98e627b88263d55c1bb355492482173088e5ea1c
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-02-01 00:39:57 +0530

    Add files via upload

---
+++ 5thprog.c
@@ -0,0 +1,18 @@
+#include<STDIO.H>
+#include<math.h>
+#define pi 3.1416
+#define max 180
+int main()
+{
+    int angle;
+    float x,y;
+    angle=0;
+    printf("Angle  cos(angle)\n\n");
+    while(angle <= max)
+    {
+        x=(pi/max)*angle;
+        y=cos(x);
+        printf("%15d %13.4f\n",angle,y);
+        angle=angle+10;
+    }
+}
