76e9941def16f1fd2f750f7e88439caab298469d
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-02-01 00:39:03 +0530

    Add files via upload

---
+++ 2ndprog.c
@@ -0,0 +1,14 @@
+/* program addition
+written by shiva*/
+#include<stdio.h>
+int main()
+{
+    int number;
+    float amount;
+
+    number=100;
+
+    amount=30.75 + 75.35;
+    printf("%d\n",number);
+    printf("%5.2f",amount);
+}
